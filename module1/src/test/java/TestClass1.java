17c85d0db67a7b45e418dac4b3e81998fa416990
Jeetendra Kukreja <jkukreja@paxata.com>
2019-08-08 14:20:06 -0700

    More updates to get tests to run in parallel

--- module1/src/test/java/TestClass1.java
+++ module1/src/test/java/TestClass1.java
@@ -2,12 +2,12 @@ import org.junit.Test;
 
 public class TestClass1 {
     @Test
-    public void TestMehod1() {
+    public void TestMehod1() throws Exception {
         new SampleClass1().SampleMethod1();
     }
 
     @Test
-    public void TestMehod2() {
+    public void TestMehod2() throws Exception {
         new SampleClass1().SampleMethod2();
     }
 }
--- module1/src/test/java/TestClass3.java
+++ module1/src/test/java/TestClass3.java
@@ -2,12 +2,12 @@ import org.junit.Test;
 
 public class TestClass3 {
     @Test
-    public void TestMehod1() {
+    public void TestMehod1() throws Exception {
         new SampleClass2().SampleMethod1();
     }
 
     @Test
-    public void TestMehod2() {
+    public void TestMehod2() throws Exception {
         new SampleClass2().SampleMethod2();
     }
 }
