49effac364cce0edc72a689e5018f76ef54ce511
Jeetendra Kukreja <jkukreja@paxata.com>
2019-08-08 15:20:37 -0700

    more tests

---
+++ module1/src/test/java/TestClass7.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class TestClass7 {
+    @Test
+    public void TestMehod1() throws Exception {
+        new SampleClass2().SampleMethod1();
+    }
+
+    @Test
+    public void TestMehod2() throws Exception {
+        new SampleClass2().SampleMethod2();
+    }
+}
